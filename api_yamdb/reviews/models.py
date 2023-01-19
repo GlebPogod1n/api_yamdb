b3d8ec24c1a3633a2275c04014bdaa9bebe8580d
Руслан Трофимов <edmondkoko777@gmail.com>
2023-01-19 19:14:46 +0300

    Добавлены модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Genre(models.Model):
@@ -15,3 +18,47 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(db_index=True, max_length=100)
+    year = models.IntegerField()
+    description = models.TextField(max_length=200, null=True, blank=True)
+    genre = models.ManyToManyField(Genre, related_name='titles', blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='reviews')
+    title = models.ForeignKey(Title, on_delete=models.CASCADE,
+                              related_name='reviews')
+    text = models.TextField(blank=False)
+    score = models.IntegerField('Оценка')
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True)
+
+    class Meta:
+        ordering = ['-pub_date', ]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE,
+                               related_name='comments')
+    text = models.TextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='comments')
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True,)
+
+    class Meta:
+        ordering = ['-pub_date']
