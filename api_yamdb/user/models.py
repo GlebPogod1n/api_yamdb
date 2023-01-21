ad46233c1eee8f80df7e0566dfd9c1995a286ba3
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-01-21 12:41:54 +0300

    Update models.py

--- api_yamdb/user/models.py
+++ api_yamdb/user/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from .validators import year_validator
 
 from .validators import year_validator
 
