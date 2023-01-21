51722f1ad152c948fb1d231c1ef7be0569db5cbb
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-01-22 00:19:19 +0300

    Update models.py

--- api_yamdb/user/models.py
+++ api_yamdb/user/models.py
@@ -3,7 +3,7 @@ from django.core.validators import EmailValidator, RegexValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-
+from .validators import year_validator
 
 class User(AbstractUser):
     """Класс модели пользователя"""
